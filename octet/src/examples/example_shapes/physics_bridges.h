bc22760efc29a3bdc531bbe868d4fa02e7dd32ff
Adam Joyce <adamjoyce@outlook.com>
2015-11-01 14:38:00 +0000

    Collision callback initial attempt

--- octet/src/examples/example_shapes/physics_bridges.h
+++ octet/src/examples/example_shapes/physics_bridges.h
@@ -6,6 +6,7 @@ namespace octet {
   class physics_bridges : public app {
     // scene for drawing objects
     ref<visual_scene> app_scene;
+    btDynamicsWorld *dynamics_world;
     dynarray<mesh_instance*> mesh_instances;
     dynarray<btRigidBody*> rigid_bodies;
     random rand;
@@ -31,9 +32,9 @@ namespace octet {
       // x position of the second spring platform
       spring_platform2_x = spring_platform_x + ((plank_num + 1) * spring_plank_gap),
 
-      ground = 0,
+      //ground = 0,
 
-      first_hinge_platform,
+      first_hinge_platform = 0,
       last_hinge_platform = first_hinge_platform + 1,
 
       first_hinge_plank,
@@ -60,79 +61,20 @@ namespace octet {
       app_scene->create_default_camera_and_lights();
       app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 40, 0));
       app_scene->get_camera_instance(0)->get_node()->rotate(-45, vec3(1, 0, 0));
-      btDynamicsWorld *dynamics_world = app_scene->get_dynamics_world();
+      dynamics_world = app_scene->get_dynamics_world();
 
       // ground
-      material *ground_color = new material(vec4(0, 1, 0, 1));
+      /*material *ground_color = new material(vec4(0, 1, 0, 1));
       mat4t mat;
       mat.loadIdentity();
       app_scene->add_shape(mat, new mesh_box(vec3(200, 1, 200)), ground_color, false);
       mesh_instances.push_back(app_scene->get_mesh_instance(ground));
-      rigid_bodies.push_back(mesh_instances[ground]->get_node()->get_rigid_body());
+      rigid_bodies.push_back(mesh_instances[ground]->get_node()->get_rigid_body());*/
 
 
       // build the bridges
       create_hinge_bridge();
       create_spring_bridge();
-
-      /*// materials
-      material *red = new material(vec4(1, 0, 0, 1));
-      material *green = new material(vec4(0, 1, 0, 1));
-      material *blue = new material(vec4(0, 0, 1, 1));
-
-      // place planks
-      mat4t mat;
-      create_plank(mat, blue, true, vec3(6, 5, 0));
-      create_plank(mat, green, false, vec3(0, 10, 0));
-      create_plank(mat, red, true, vec3(0, 6, 0));
-      create_plank(mat, green, false, vec3(-9, 8, 0));
-      create_plank(mat, green, true, vec3(-6, 8, 0));
-
-      mesh_instances.push_back(app_scene->get_mesh_instance(0));
-      mesh_instances.push_back(app_scene->get_mesh_instance(1));
-      mesh_instances.push_back(app_scene->get_mesh_instance(2));
-      mesh_instances.push_back(app_scene->get_mesh_instance(3));
-      mesh_instances.push_back(app_scene->get_mesh_instance(4));
-      rigid_bodies.push_back(mesh_instances[0]->get_node()->get_rigid_body());
-      rigid_bodies.push_back(mesh_instances[1]->get_node()->get_rigid_body());
-      rigid_bodies.push_back(mesh_instances[2]->get_node()->get_rigid_body());
-      rigid_bodies.push_back(mesh_instances[3]->get_node()->get_rigid_body());
-      rigid_bodies.push_back(mesh_instances[4]->get_node()->get_rigid_body());
-
-      // hinges
-      btHingeConstraint *hinge0 = new btHingeConstraint(*rigid_bodies[0], btVector3(-1, 0.5f, 0), btVector3(0, 0, 1), false);
-      btHingeConstraint *hinge1 = new btHingeConstraint(*rigid_bodies[3], *rigid_bodies[4], btVector3(1.5f, 0, 0), btVector3(-1.5f, 0, 0),
-                                                         btVector3(0, 0, 1), btVector3(0, 0, 1));
-
-      const float PI = 3.14159f;
-      btScalar lower_angle_limit = 0;
-      btScalar upper_angle_limit = PI / 6;
-      hinge1->setLimit(lower_angle_limit, upper_angle_limit);
-
-      dynamics_world->addConstraint(hinge0);
-      dynamics_world->addConstraint(hinge1);
-
-      // generic 6DOF spring
-      // Acknowledgement: http://bullet.googlecode.com/svn/trunk/Demos/ConstraintDemo/ConstraintDemo.cpp
-      btTransform frame_in_a, frame_in_b;
-      frame_in_a = btTransform::getIdentity();
-      frame_in_a.setOrigin(btVector3(btScalar(0), btScalar(0), btScalar(0)));
-      frame_in_b = btTransform::getIdentity();
-      frame_in_b.setOrigin(btVector3(btScalar(0), btScalar(4), btScalar(0)));
-      btGeneric6DofSpringConstraint *spring = new btGeneric6DofSpringConstraint(*rigid_bodies[1], *rigid_bodies[2], frame_in_a, frame_in_b, true);
-
-      spring->setLinearUpperLimit(btVector3(0, 2, 0));
-      spring->setLinearLowerLimit(btVector3(0, -5, 0));
-
-      spring->setAngularLowerLimit(btVector3(0, 0, 0));
-      spring->setAngularUpperLimit(btVector3(0, 0, 0));
-
-      dynamics_world->addConstraint(spring, false);
-
-      spring->enableSpring(1, true);
-      spring->setStiffness(1, 19);
-      spring->setDamping(1, 0.5f);
-      spring->setEquilibriumPoint();*/
     }
 
     /// Creates a plank for a bridge.
@@ -175,11 +117,11 @@ namespace octet {
       btHingeConstraint *hinge = new btHingeConstraint(*rigid_bodies[first_hinge_platform], *rigid_bodies[first_hinge_plank],
                                                         btVector3(1.5f, bridge_height - 0.5f, 0), btVector3(-1.5f, 0, 0),
                                                         btVector3(0, 0, 1), btVector3(0, 0, 1), false);
-      app_scene->get_dynamics_world()->addConstraint(hinge);
+      dynamics_world->addConstraint(hinge);
 
       // place and hinge the remaining planks
       int i = first_hinge_plank + 1;
-      for (i; i <= last_hinge_plank; ++i) {
+      for (i; i <= last_hinge_plank; i++) {
         plank_location = vec3(plank_location[0] + hinge_plank_gap, plank_location[1], plank_location[2]); 
         create_plank(mat, plank_color, true, plank_location);
         mesh_instances.push_back(app_scene->get_mesh_instance(i));
@@ -187,18 +129,19 @@ namespace octet {
 
         hinge = new btHingeConstraint(*rigid_bodies[i-1], *rigid_bodies[i], btVector3(1.5f, 0, 0), btVector3(-1.5f, 0, 0),
                                        btVector3(0, 0, 1), btVector3(0, 0, 1), false);
-        app_scene->get_dynamics_world()->addConstraint(hinge);
+        dynamics_world->addConstraint(hinge);
 
         // deal with the last plank to platform hinge
         if (i == last_hinge_plank) {
           hinge = new btHingeConstraint(*rigid_bodies[i], *rigid_bodies[last_hinge_platform], btVector3(1.5f, 0, 0), 
                                          btVector3(-1.5f, bridge_height - 0.5f, 0), btVector3(0, 0, 1), btVector3(0, 0, 1), false);
-          app_scene->get_dynamics_world()->addConstraint(hinge);
+          dynamics_world->addConstraint(hinge);
         }
       }
     }
 
     /// Assemble spring bridge.
+    /// Acknowledgement: http://bullet.googlecode.com/svn/trunk/Demos/ConstraintDemo/ConstraintDemo.cpp
     void create_spring_bridge() {
       material *platform_color = new material(vec4(1, 0, 0, 1));
       material *plank_color = new material(vec4(1, 1, 0, 1));
@@ -251,6 +194,33 @@ namespace octet {
       }
     }
 
+    /// Detect collisions.
+    /// Acknowledgement: http://hamelot.co.uk/programming/using-bullet-only-for-collision-detection/
+    void handle_collisions() {
+      // perform collision detection
+      dynamics_world->performDiscreteCollisionDetection();
+      int num_manifolds = dynamics_world->getDispatcher()->getNumManifolds();
+
+      // for each contact manifold
+      for (int i = 0; i < num_manifolds; i++) {
+        btPersistentManifold *contact_manifold = dynamics_world->getDispatcher()->getManifoldByIndexInternal(i);
+        const btCollisionObject *object_a = static_cast<const btCollisionObject*>(contact_manifold->getBody0());
+        const btCollisionObject *object_b = static_cast<const btCollisionObject*>(contact_manifold->getBody1());
+        contact_manifold->refreshContactPoints(object_a->getWorldTransform(), object_b->getWorldTransform());
+        int num_contacts = contact_manifold->getNumContacts();
+
+        // for each contact point in that manifold
+        for (int j = 0; j < num_contacts; j++) {
+          // get the contact information
+          btManifoldPoint &point = contact_manifold->getContactPoint(j);
+          btVector3 point_a = point.getPositionWorldOnA();
+          btVector3 point_b = point.getPositionWorldOnB();
+          double point_dist = point.getDistance();
+          printf("CONTACT");
+        }
+      }
+    }
+
     /// Called to draw the world.
     void draw_world(int x, int y, int w, int h) {
       int vx = 0, vy = 0;
@@ -259,10 +229,13 @@ namespace octet {
 
       // used to test spring
       mat4t mat;
-      material *color = new material(vec4(1, 1, 1, 1));
+      material *color = new material(vec4(0, 0, 0, 1));
       float r = rand.get(first_spring_plank, last_spring_plank);
       if (is_key_down(key_space)) {
-        rigid_bodies[r]->applyForce(btVector3(0, -200, 0), btVector3(0, 0, 0));
+        mat.loadIdentity();
+        mat.translate(vec3(-6, 30, hinge_platform_z));
+        app_scene->add_shape(mat, new mesh_sphere(), color, true);
+        //rigid_bodies[r]->applyForce(btVector3(0, -200, 0), btVector3(0, 0, 0));
       }
 
       // update matrices, assume 30fps
@@ -270,6 +243,8 @@ namespace octet {
 
       // draw the scene
       app_scene->render((float)vx / vy);
+
+      handle_collisions();
     }
   };
 }
