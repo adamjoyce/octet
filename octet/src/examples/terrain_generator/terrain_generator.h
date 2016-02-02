2645e44368a41c3cdb806c6582d82fff989945ec
Adam Joyce <adamjoyce@outlook.com>
2016-02-02 20:59:03 +0000

    Added text overlay for noise information and controls

--- octet/src/examples/terrain_generator/terrain_generator.h
+++ octet/src/examples/terrain_generator/terrain_generator.h
@@ -24,11 +24,14 @@ namespace octet {
       app_scene->get_camera_instance(0)->get_node()->translate(vec3(grid_width * 0.5f, grid_height * 0.5f, 400));
       //app_scene->get_camera_instance(0)->get_node()->rotate(90, vec3(0, 1, 0));
 
-      /*material *red = new material(vec4(1, 0, 0, 1));
-      mesh_box *box = new mesh_box(vec3(4, 4, 0));
-      scene_node *node = new scene_node();
-      app_scene->add_child(node);
-      app_scene->add_mesh_instance(new mesh_instance(node, box, red));*/
+      // Overlay text.
+      aabb bb0(vec3(-210, 200, 0), vec3(160, 150, 0));
+      aabb bb1(vec3(-220, -270, 0), vec3(150, 80, 0));
+      overlay = new text_overlay();
+      noise_information = new mesh_text(overlay->get_default_font(), "", &bb0);
+      control_information = new mesh_text(overlay->get_default_font(), controls, &bb1);
+      overlay->add_mesh_text(noise_information);
+      overlay->add_mesh_text(control_information);
 
       noise noise;
       noise.initialise_perms();
@@ -51,10 +54,10 @@ namespace octet {
       dc = GetDC(console);
 
       std::vector<int> height_line(grid_width, 0);
-      draw_height_line(noise, height_line);
+      //draw_height_line(noise, height_line);
 
       // Populate the luminance values for the grid.
-      pixel_luminance(noise, luminance);
+      //pixel_luminance(noise, height_line, luminance);
 
       // Set up the height landscape array.
       /*random rand;
@@ -79,6 +82,10 @@ namespace octet {
       // draw the scene
       app_scene->render((float)vx / vy);
 
+      handle_input();
+
+      update_text(vx, vy);
+
       // Tumble the box (there is only one mesh instance).
       //scene_node *node = app_scene->get_mesh_instance(0)->get_node();
       //node->rotate(1, vec3(1, 0, 0));
@@ -86,14 +93,30 @@ namespace octet {
     }
 
   private:
+    // For drawing text.
+    ref<text_overlay> overlay;
+    ref<mesh_text> noise_information;
+    ref<mesh_text> control_information;
+
+    // For overlay display.
+    string controls = "Controls:\n"
+      "Load Files      = F1 - F8\n"
+      "+/- Iterate     = SPACE/BCKSPACE\n"
+      "+/- Angle       = TAB/CTRL\n"
+      "+/- Line Width  = INS/DEL\n"
+      "+/- Line Length = F9/ESC\n"
+      "+/- Zoom        = RIGHT/LEFT ARROW\n"
+      "Move Up/Down    = UP/DOWN ARROW\n";
+
     // Private variables.
     const int grid_height = 256;
     const int grid_width = 256;
 
     // Noise variables.
-    const float scale_factor = 0.09f;
+    const float scale_factor = 0.007f;
     const int iterations = 16;
     const float persistense = 0.5f;
+    const int luminance_threshold = 128;
 
     // 
     int max_height = 0;
@@ -108,15 +131,16 @@ namespace octet {
     HDC dc;
  
     // Determine the luminance for each pixel in the 2D grid (and draw them in Windows console).
-    void pixel_luminance(noise &noise, std::vector<std::vector<int>> &luminance) {
-      for (int i = 0; i < grid_height; i++) {
-        for (int j = 0; j < grid_width; j++) {
-          luminance[j][i] = noise.fBM(iterations, j, i, persistense, scale_factor, 0, 255);
-          if (i < min_height && luminance[j][i] >= 115) {
-            create_ground_tile(vec3(j, i, 0), ground, false);
+    void pixel_luminance(noise &noise, std::vector<int> &height_line, std::vector<std::vector<int>> &luminance) {
+      bool height_reached = false;
+      for (int i = 0; i < grid_width; i++) {
+        for (int j = 0; j < grid_height; j++) {
+          luminance[i][j] = noise.fBM(16, i, j, persistense, scale_factor, 0, 255);
+          if (j <= height_line[i] && luminance[i][j] >= luminance_threshold) {
+            create_ground_tile(vec3(i, j, 0), ground, false);
           }
           // WINDOWS ONLY.
-          SetPixel(dc, j, i, RGB(luminance[j][i], luminance[j][i], luminance[j][i]));
+          SetPixel(dc, j, i, RGB(luminance[i][j], luminance[i][j], luminance[i][j]));
           //printf("%i ", luminance);
         }
         //printf("\n");
@@ -127,7 +151,7 @@ namespace octet {
     //Draws the height line for the terrain in the windows console.
     void draw_height_line(noise &noise, std::vector<int> &height_line) {
       for (int i = 0; i < grid_width; i++) {
-        height_line[i] = noise.fBM(iterations, 0, i, 0.4f, 0.003f, 0, 255);
+        height_line[i] = noise.fBM(iterations, 0, i, 0.5f, 0.007f, 0, 255);
         if (height_line[i] > max_height) {
           max_height = height_line[i];
         } else if (height_line[i] < min_height) {
@@ -149,7 +173,7 @@ namespace octet {
           }
           else {
             SetPixel(dc, i + 255, j, RGB(0, 0, 0));
-            create_ground_tile(vec3(i, j, 0), ground, false);
+            //create_ground_tile(vec3(i, j, 0), ground, false);
             red = 1;
           }
           //printf("%i, %i, %i\n", i, j, red);
@@ -165,6 +189,42 @@ namespace octet {
       mat.loadIdentity();
       mat.translate(location);
       app_scene->add_shape(mat, new mesh_box(vec3(0.5, 0.5, 0)), color, is_dynamic);
+    }
+
+    /// Detect inputs for a number of actions on the terrain.
+    void handle_input() {
+      
+    }
+
+    /// Update the terrain information on the text overlay.
+    void update_text(int vx, int vy)
+    {
+      noise_information->clear();
+
+      // Write text.
+      char buffer[5][256];
+      sprintf(buffer[0], "%i", iterations);
+      sprintf(buffer[1], "%.4f", scale_factor);
+      sprintf(buffer[2], "%.4f", persistense);
+      sprintf(buffer[3], "%i", luminance_threshold);
+
+      // Format the mesh.
+      noise_information->format("Noise Information:\n"
+                                "Iterations:                %s\n"
+                                "Scale Factor / Frequency:  %s\n"
+                                "Persistence:               %s\n"
+                                "Luminance Threshold:       %s\n",
+        buffer[0],
+        buffer[1],    
+        buffer[2],
+        buffer[3],
+        buffer[4]);
+
+      // Update the OpenGL geometry.
+      noise_information->update();
+
+      // Render the overlay.
+      overlay->render(vx, vy);
     }
   };
 }
